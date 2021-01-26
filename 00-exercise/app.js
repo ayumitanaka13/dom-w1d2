f809d66901d44617f25701a709023396b769daf2
ayumitanaka <tanakayumi13@gmail.com>
2021-01-26 11:50:07 -0800

    assignment

--- 00-exercise/app.js
+++ 00-exercise/app.js
@@ -1,13 +1,12 @@
 const form = document.querySelector('#tweetForm');
-
-const nameInput = document.querySelector('#name');
-const tweetInput = document.querySelector('#tweet');
+// const nameInput = document.querySelector('#name');
+// const tweetInput = document.querySelector('#tweet');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
-    li.innerHTML = "<b>" + nameInput.value + "</b>" + "-" + tweetInput.value;
+    li.innerHTML = "<b>" + form.username.value + "</b>" + "-" + form.tweet.value;
     ul.appendChild(li);
 });
